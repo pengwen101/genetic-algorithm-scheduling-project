fdd77980e688ed7f0daf4cb761004d01bc78c52f
pengwen101 <111033605+pengwen101@users.noreply.github.com>
2024-04-13 20:22:28 +0700

    fcpd

--- ga.py
+++ ga.py
@@ -20,6 +20,7 @@ class GASchedule:
     population = None
     generation = None
     machine = ["M1", "M2", "M3", "M4"]
+    initial_size = None
 
 
     def __init__(self, population_size, crossover_rate, mutation_rate):
@@ -41,35 +42,37 @@ class GASchedule:
                         duration[k] -= 1
                     else:
                         if(len(chosen) == len(product_list)):
-                            subchromosome.append(product0.id)
+                            subchromosome.append(product0)
                         else:
                             chosen_index = random.choice(range(len(product_list)))
                             while(chosen.count(chosen_index)):
                                 chosen_index = random.choice(range(len(product_list)))
-                            subchromosome.append(product_list[chosen_index].id)
+                            subchromosome.append(product_list[chosen_index])
                             chosen.append(chosen_index)
                             duration[k] = product_list[chosen_index].duration
                 chromosome.append(subchromosome)
             self.population.append(chromosome)
+        self.initial_size = len(product_list)-1
     
     def print_population(self):
         for i in range(len(self.population)):
             print("Chromosome")
             for j in range(len(self.population[i])):
                 for k in range(len(self.population[i][j])):
-                    print(self.population[i][j][k], end = " ")
+                    print(self.population[i][j][k].id, end = " ")
                 print()
             print("FCMS:", self.fcms(self.population[i]))
+            print("FCPD:", self.fcpd(self.population[i]))
     
     def fcms(self,chromosome):
         zero_count = [0,0,0,0]
         fcms = 0
         for i in range(14):
             for j in range(4):
-                if (chromosome[i][j] == "0"):
+                if (chromosome[i][j].id == "0"):
                     if(zero_count[j] == 2):
                         continue
-                    if(i != 0 and chromosome[i-1][j] == "0"):
+                    if(i != 0 and chromosome[i-1][j].id == "0"):
                         zero_count[j] += 1
                     else:
                         zero_count[j] == 1
@@ -78,14 +81,22 @@ class GASchedule:
                 fcms+=0.25
         return fcms
 
-    # def fcpd(self, chromosome):
-    #     duration = []
-    #     for i in range(14):
-    #         for j in range(4):
-    #             if(chromosome[i][j].machine.count(self.machine[j])):
-    #                 duration.append(chromosome[i][j].duration)
+    def fcpd(self, chromosome):
+        duration = [0,0,0,0]
+        score = 0
+        for i in range(14):
+            for j in range(4):
+                if(chromosome[i][j].machine.count(self.machine[j])):
+                    if(i != 0 and chromosome[i-1][j] == chromosome[i][j] and duration[j] != 1):
+                        duration[j] -= 1
+                    else:
+                        duration[j] = chromosome[i][j].duration
+                    if(duration[j] == 1):
+                        score+=1
+        #duration = [3, 0, 0, 0]     
+        return score/self.initial_size
 
-    # #{3, 2, 2, 2, 1, 1, 1}
+    #{3, 2, 2, 2, 1, 1, 1}
 
     
     # def fitness_value(self, chromosome):
@@ -94,11 +105,11 @@ class GASchedule:
 
             
 
-        #Efficiency per machine
-        #evaluate through every machine, count the number of products (right ones)
-        #rata2 differences per machine paling small
+    #     Efficiency per machine
+    #     evaluate through every machine, count the number of products (right ones)
+    #     rata2 differences per machine paling small
 
-        #
+        
 
     
     # def select_best(self):
@@ -112,10 +123,10 @@ class GASchedule:
 
 sched = GASchedule(100, 0.1,0.1)
 
-product0 = Product("0", 0, 0, [])
-product1 = Product("B1", 1, 150000, ["M1"])
-product2 = Product("B2", 2, 200000, ["M1"])
-product3 = Product("B3", 3, 220000, ["M1"])
+product0 = Product("0", 0, 0, ["M1", "M2", "M3", "M4"])
+product1 = Product("B1", 1, 150000, ["M1", "M3"])
+product2 = Product("B2", 2, 200000, ["M1", "M3"])
+product3 = Product("B3", 3, 220000, ["M1", "M3"])
 product4 = Product("B4", 2, 300000, ["M1", "M4"])
 product5 = Product("B5", 1, 430000, ["M1", "M4"])
 product6 = Product("B6", 4, 345000, ["M2", "M4"])
@@ -129,5 +140,7 @@ product12 = Product("B12", 5, 125000, ["M3"])
 sched.create_population([product0, product1, product2, product3, product4, product1, product2, product3, product4, product1, product2, product3, product4, product1, product2, product3, product4])
 
 sched.print_population()
+
+sched.fcpd()
 
 

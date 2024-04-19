2ac0430ac2c7fb076b04183fbe0f861211509ba3
pengwen101 <111033605+pengwen101@users.noreply.github.com>
2024-04-19 22:28:26 +0700

    Update ga.py

--- ga.py
+++ ga.py
@@ -21,17 +21,17 @@ class GASchedule:
     crossover_rate = None
     mutation_rate = None
     population = None
-    generation = None
+    generation_count = None
     machine = ["M1", "M2", "M3", "M4"]
     initial_size = None
     initial_product = {}
 
-    max_revenue = 0
 
-    def __init__(self, population_size, crossover_rate, mutation_rate):
+    def __init__(self, population_size, crossover_rate, mutation_rate, generation_count):
         self.population_size = population_size
         self.crossover_rate = crossover_rate
         self.mutation_rate = mutation_rate
+        self.generation_count = generation_count
     
     def create_population(self, product_list):
         for product in product_list:
@@ -75,8 +75,22 @@ class GASchedule:
             print("FCMS:", self.fcms(self.population[i]))
             print("FCPD:", self.fcpd(self.population[i]))
             print("FCPQA:", self.fcpqa(self.population[i]))
-            print("FCR: ", self.fcr(self.population[i]))
-            # print(self.max_revenue)
+
+    def selection(self, scores):
+        selected_ix = random.randint(0, self.population_size-1)
+        for ix in random.randint(0, self.population_size-1, 2):
+            if scores[ix] > scores[selected_ix]:
+                selected_ix = ix
+        
+        return(self.population[selected_ix])
+
+    def scores(self):
+        total_score = list()
+        for i in range (self.population_size):
+            total_score[i] = self.fcms(self.population[i]) + self.fcpd(self.population[i]) + self.fcms(self.population[i]) + self.fcfwe(self.population[i]) + self.fcr(self.population[i])
+        
+        return (total_score)
+    
     
     def fcms(self,chromosome):
         zero_count = [0,0,0,0]
@@ -109,6 +123,27 @@ class GASchedule:
                         score+=1
         #duration = [3, 0, 0, 0]     
         return score/self.initial_size
+
+    def fcr(self, chromosome):
+        duration = [0,0,0,0]
+        total_revenue = 0
+        for i in range(14):
+            for j in range(4):
+                if(chromosome[i][j].machine.count(self.machine[j])):
+                    if(i != 0 and chromosome[i-1][j] == chromosome[i][j] and duration[j] != 1):
+                        duration[j] -= 1
+                    else:
+                        duration[j] = chromosome[i][j].duration
+                    if(duration[j] == 1):
+                        # print("count", end=" ")
+                        total_revenue += chromosome[i][j].price
+        if(total_revenue > self.max_revenue):
+            self.max_revenue = total_revenue
+            print("yes")
+        
+        print(total_revenue)
+        print(self.max_revenue)
+        return total_revenue/self.max_revenue
     
     def fcpqa(self, chromosome):
         duration = [0,0,0,0]
@@ -132,50 +167,38 @@ class GASchedule:
 
         return(score/(total_count-1))
 
-    
-    def fcr(self, chromosome):
-        duration = [0,0,0,0]
-        total_revenue = 0
-        for i in range(14):
-            for j in range(4):
-                if(chromosome[i][j].machine.count(self.machine[j])):
-                    if(i != 0 and chromosome[i-1][j] == chromosome[i][j] and duration[j] != 1):
-                        duration[j] -= 1
-                    else:
-                        duration[j] = chromosome[i][j].duration
-                    if(duration[j] == 1):
-                        # print("count", end=" ")
-                        total_revenue += chromosome[i][j].price
-        if(total_revenue > self.max_revenue):
-            self.max_revenue = total_revenue
-        return total_revenue/self.max_revenue
+    def run(self, input):
+        self.create_population(input)
+        best = self.population[0]
+        best_eval = self.scores()[0]
 
-    #{3, 2, 2, 2, 1, 1, 1}
-
-    
-    # def fitness_value(self, chromosome):
-    #     #Maintenance schedule
-    #     return self.fcms(chromosome)
-
+        for gen in range(self.generation_count):
+            scores = self.scores()
+            for i in range(self.population_size):
+                if scores[i] < best_eval:
+                    best = self.population[i]
+                    best_eval = scores[i]
+                    print("Gen %d, new best:")
+                    for row in range(14):
+                        for column in range(4):
+                            print(best[row][column], end=" ")
+                        print()
+                    print("Fitness =", best_eval)
             
+            selected = [self.selection(self.population, scores) for _ in range(self.population_size)]
 
-    #     Efficiency per machine
-    #     evaluate through every machine, count the number of products (right ones)
-    #     rata2 differences per machine paling small
+            children = list()
+            for i in range(0, self.population_size, 2):
+                p1, p2 = selected[i], selected[i+1]
+                for c in self.crossover(p1, p2, self.crossover_rate):
+                    self.mutation(c, self.mutation_rate)
+                    children.append(c)
+            
+            self.population = children
+        return [best, best_eval]
 
-        
 
-    
-    # def select_best(self):
-
-        
-    
-    # def genetic_algorithm(self, product_list):
-    #     for i in range(self.generation):
-    #         self.create_population(product_list)
-    #         selected_chromosome = self.select_best()
-
-sched = GASchedule(100, 0.1,0.1)
+sched = GASchedule(100, 0.1,0.1, 500)
 
 product0 = Product("0", 0, 0, ["M1", "M2", "M3", "M4"])
 product1 = Product("B1", 1, 150000, ["M1", "M3"])
@@ -191,7 +214,7 @@ product10 = Product("B10", 2, 300000, ["M2"])
 product11 = Product("B11", 4, 175000, ["M3"])
 product12 = Product("B12", 5, 125000, ["M3"])
 
-sched.create_population([product0, product1, product2, product3, product4, product1, product2, product3, product4, product1, product2, product3, product4, product1, product2, product3, product4])
+sched.run([product0, product1, product2, product3, product4, product1, product2, product3, product4, product1, product2, product3, product4, product1, product2, product3, product4])
 
 sched.print_population()
 
